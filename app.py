979b60fa307f6697f7dffb094b40ae191d2122ad
acidjazzer2010-boop <acidjazzer2010@gmail.com>
2026-07-31 11:02:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def verify_credentials(username, password):
     return False, None, None
 
 if not st.session_state.authenticated:
-    st.markdown("<h2 style='text-align: center; color: #642A38;'>🔐 Личный кабинет KRAYVIN</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: #642A38;'>🔐 Личный кабинет </h2>", unsafe_allow_html=True)
     col1, col2, col3 = st.columns([1, 1.2, 1])
     with col2:
         with st.form("login_form"):
